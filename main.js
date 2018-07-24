ea042c9c5c71adc1a047643cf0d2a181b5e77474
liuhantao <1045543977@qq.com>
2018-07-24 10:25:58 +0800

    测试

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /**
  * Created by liuhantao on 2018/7/24.
  */
-var model =require('./dist/bundle-6b7f446665d7ee6ecb08.js');
+import model from'./dist/bundle-0856776882aa6c056abd.js';
 export default model
