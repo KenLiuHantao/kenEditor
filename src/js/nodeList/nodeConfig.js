f7676dbac454132f677e9a57ea336954129f08db
liuhantao <1045543977@qq.com>
2018-08-17 14:44:54 +0800

    微调样式

--- src/js/nodeList/nodeConfig.js
+++ src/js/nodeList/nodeConfig.js
@@ -7,26 +7,26 @@ let config=[
         icon:'',
         type:'firstLv',
         child:[
-            {
-                name:'KAFKA',
-                icon:'&#xe65a;',
-                type:'sourceData',
-                background:'rgba(0,0,0,0.1)',
-                iconColor:'blue'
-            },
-            {
-                name:'ElasticSearch',
-                icon:'&#xe80f;',
-                type:'sourceData'
-            },{
-                name:'Hive',
-                icon:'&#xe601;',
-                type:'sourceData'
-            },{
-                name:'Hbase',
-                icon:null,
-                type:'sourceData'
-            }
+            //{
+            //    name:'KAFKA',
+            //    icon:'&#xe65a;',
+            //    type:'sourceData',
+            //    background:'rgba(0,0,0,0.1)',
+            //    iconColor:'blue'
+            //},
+            //{
+            //    name:'ElasticSearch',
+            //    icon:'&#xe80f;',
+            //    type:'sourceData'
+            //},{
+            //    name:'Hive',
+            //    icon:'&#xe601;',
+            //    type:'sourceData'
+            //},{
+            //    name:'Hbase',
+            //    icon:null,
+            //    type:'sourceData'
+            //}
         ]
     },
     {
@@ -34,23 +34,23 @@ let config=[
         icon:'',
         type:'firstLv',
         child:[
-            {
-                name:'算子1',
-                icon:"&#xe629;",
-                type:'tools'
-            },{
-                name:'算子2',
-                icon:null,
-                type:'tools'
-            },{
-                name:'算子3',
-                icon:null,
-                type:'tools'
-            },{
-                name:'算子4',
-                icon:null,
-                type:'tools'
-            }
+            //{
+            //    name:'算子1',
+            //    icon:"&#xe629;",
+            //    type:'tools'
+            //},{
+            //    name:'算子2',
+            //    icon:null,
+            //    type:'tools'
+            //},{
+            //    name:'算子3',
+            //    icon:null,
+            //    type:'tools'
+            //},{
+            //    name:'算子4',
+            //    icon:null,
+            //    type:'tools'
+            //}
         ]
     },
     {
@@ -58,24 +58,24 @@ let config=[
         icon:'',
         type:'firstLv',
         child:[
-            {
-                name:'KAFKA',
-                icon:'&#xe65a;',
-                type:'targetData'
-            },
-            {
-                name:'ElasticSearch',
-                icon:'&#xe80f;',
-                type:'targetData'
-            },{
-                name:'Hive',
-                icon:'&#xe601;',
-                type:'targetData'
-            },{
-                name:'Hbase',
-                icon:'&#xe6e7;',
-                type:'targetData'
-            }
+            //{
+            //    name:'KAFKA',
+            //    icon:'&#xe65a;',
+            //    type:'targetData'
+            //},
+            //{
+            //    name:'ElasticSearch',
+            //    icon:'&#xe80f;',
+            //    type:'targetData'
+            //},{
+            //    name:'Hive',
+            //    icon:'&#xe601;',
+            //    type:'targetData'
+            //},{
+            //    name:'Hbase',
+            //    icon:'&#xe6e7;',
+            //    type:'targetData'
+            //}
         ]
     }
 ];
